0913d85482fe56af6167c38da16ad15ca7257a7a
Prityanshu Singh <prityanshusingh2003@gmail.com>
2024-08-14 04:36:21 +0530

    Added Vercel Analytics

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,8 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import reportWebVitals from './reportWebVitals';
+import { Analytics } from "@vercel/analytics/react";
 import RecoveryContextProvider from "./context/RecoveryContext.jsx";
 import "./index.scss";
 
@@ -13,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <RecoveryContextProvider>
         <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
           <BrowserRouter>
+            <Analytics />
             <App />
           </BrowserRouter>
         </GoogleOAuthProvider>
@@ -20,3 +23,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </AuthContextProvider>
   </React.StrictMode>
 );
+
+reportWebVitals();
