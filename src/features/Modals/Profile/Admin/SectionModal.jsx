b712b2a3f41239c5c80086f8048f1d0fe5a4fff8
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2025-01-14 02:07:58 +0530

    fixing form css

--- src/features/Modals/Profile/Admin/SectionModal.jsx
+++ src/features/Modals/Profile/Admin/SectionModal.jsx
@@ -3,8 +3,8 @@ import styles from "./styles/Preview.module.scss";
 
 const Section = ({ section, handleChange }) => {
   const getInputFields = (field) => {
-    const valiedTypes = ["checkbox", "radio"];
-    if (valiedTypes.includes(field.type)) {
+    const validTypes = ["checkbox", "radio"];
+    if (validTypes.includes(field.type)) {
       const valueToArray = field.value.split(",");
       return valueToArray.map((value, index) => (
         <div
@@ -18,30 +18,31 @@ const Section = ({ section, handleChange }) => {
             label={value}
             showLabel={false}
             type={field.type}
-            value={value}
+            value={field.onChangeValue || ""}
             name={field.name}
             onChange={(e) => handleChange(field, e.target.value)}
           />
         </div>
       ));
     }
+    return null;
   };
 
   const getTeamFields = () => {
     const data = [];
     if (section.name === "Team Members") {
-      section.fields.forEach((field, index) => {
-        if (index % 3 === 0) {
-          const team = section.fields.slice(index, index + 3);
-          data.push(team);
-        }
-      });
-      return data;
+      for (let i = 0; i < section.fields.length; i += 3) {
+        data.push(section.fields.slice(i, i + 3));
+      }
     }
+    return data;
   };
 
   const renderTeamFields = () => {
-    return getTeamFields().map((team, index) => (
+    const teams = getTeamFields();
+    if (!teams.length) return null;
+
+    return teams.map((team, index) => (
       <div
         key={index}
         style={{
@@ -51,34 +52,35 @@ const Section = ({ section, handleChange }) => {
         }}
         className={styles.teamContainer}
       >
-        {team.map((field, index) => (
+        {team.map((field, idx) => (
           <div
-            key={index}
+            key={idx}
             style={{
               width: "30%",
             }}
             className={styles.teamField}
           >
             <Input
-              placeholder={field.value}
+              placeholder={field.value || ""}
               label={`${field.name} ${field.isRequired ? "*" : ""}`}
               type={field.type}
               name={field.name}
               style={{ width: "100%" }}
               value={
                 field.type === "file" || field.type === "image"
-                  ? field.onChangeValue?.name
-                  : field.onChangeValue
+                  ? field.onChangeValue?.name || ""
+                  : field.onChangeValue || ""
               }
               onChange={(e) => {
                 const val = field.type === "select" ? e : e.target.value;
                 handleChange(field, val);
               }}
               options={
-                field.type === "select"
-                  ? field.value.split(",").map((option) => {
-                      return { value: option, label: option };
-                    })
+                field.type === "select" && field.value
+                  ? field.value.split(",").map((option) => ({
+                      value: option,
+                      label: option,
+                    }))
                   : []
               }
             />
@@ -93,51 +95,65 @@ const Section = ({ section, handleChange }) => {
       {section.name === "Team Members" && renderTeamFields()}
       {section.name !== "Team Members" &&
         section.fields.length > 0 &&
-        section.fields.map(
-          (field) =>
-            field !== undefined && (
-              <div key={field._id}>
-                {field.type !== "checkbox" && field.type !== "radio" ? (
+        section.fields.map((field) => {
+          if (!field) return null;
+          return (
+            <div key={field._id}>
+              {field.type !== "checkbox" && field.type !== "radio" ? (
+                <div
+                style={{
+                  width:
+                    field.type === "select"
+                      ? window.innerWidth < 500
+                        ? "250px"
+                        : "300px"
+                      : "100%",
+                  margin: "0 auto",
+                }}               
+                
+                >
                   <Input
                     placeholder={
                       field.type === "select"
                         ? `Choose ${field.name}`
-                        : field.value
+                        : field.value || ""
                     }
                     label={`${field.name} ${field.isRequired ? "*" : ""}`}
                     type={field.type}
                     name={field.name}
                     value={
                       field.type === "file" || field.type === "image"
-                        ? field.onChangeValue?.name
-                        : field.onChangeValue
+                        ? field.onChangeValue?.name || ""
+                        : field.onChangeValue || ""
                     }
                     onChange={(e) => {
                       const val = field.type === "select" ? e : e.target.value;
                       handleChange(field, val);
                     }}
                     options={
-                      field.type === "select"
-                        ? field.value.split(",").map((option) => {
-                            return { value: option, label: option };
-                          })
+                      field.type === "select" && field.value
+                        ? field.value.split(",").map((option) => ({
+                            value: option,
+                            label: option,
+                          }))
                         : []
                     }
                   />
-                ) : (
-                  <label
-                    style={{
-                      color: "#fff",
-                      fontSize: ".8em",
-                    }}
-                  >
-                    {field.name}
-                  </label>
-                )}
-                {getInputFields(field)}
-              </div>
-            )
-        )}
+                </div>
+              ) : (
+                <label
+                  style={{
+                    color: "#fff",
+                    fontSize: ".8em",
+                  }}
+                >
+                  {field.name}
+                </label>
+              )}
+              {getInputFields(field)}
+            </div>
+          );
+        })}
     </div>
   );
 };
